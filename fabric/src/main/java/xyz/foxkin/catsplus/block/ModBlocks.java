07c53c6e908040243bcd86f0a0889321cfd49ffc
fox <99151137+fox-lol@users.noreply.github.com>
2022-06-21 22:50:37 +0100

    feat(blocks): added cat block

---
+++ fabric/src/main/java/xyz/foxkin/catsplus/block/ModBlocks.java
@@ -0,0 +1,33 @@
+package xyz.foxkin.catsplus.block;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import xyz.foxkin.catsplus.commonside.fabric.CatsPlusFabric;
+
+import net.minecraft.util.registry.Registry;
+
+public class ModBlocks {
+
+    public static final Block CAT_BLOCK = registerBlock("cat_block",
+            new Block(FabricBlockSettings.of(Material.METAL).strength(6f).requiresTool()), ItemGroup.MISC);
+
+    private static Block registerBlock(String name, Block block, ItemGroup group){
+        registerBLockItem(name, block, group);
+        return Registry.register(Registry.BLOCK, new Identifier(CatsPlusFabric.MOD_ID, name), block);
+    }
+
+    private static Item registerBLockItem(String name, Block block, ItemGroup group){
+        return Registry.register(Registry.ITEM, new Identifier(CatsPlusFabric.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings().group(group)));
+    }
+
+    public static void registerModBlocks(){
+        CatsPlusFabric.LOGGER.info("Registering ModBlocks for " + CatsPlusFabric.MOD_ID);
+    }
+}
--- fabric/src/main/java/xyz/foxkin/catsplus/commonside/fabric/CatsPlusFabric.java
+++ fabric/src/main/java/xyz/foxkin/catsplus/commonside/fabric/CatsPlusFabric.java
@@ -1,12 +1,23 @@
 package xyz.foxkin.catsplus.commonside.fabric;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import xyz.foxkin.catsplus.block.ModBlocks;
 import xyz.foxkin.catsplus.commonside.CatsPlus;
 import net.fabricmc.api.ModInitializer;
 
+
+
 public class CatsPlusFabric implements ModInitializer {
+    public static final String MOD_ID = "catsplus";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
 
     @Override
     public void onInitialize() {
         CatsPlus.init();
+
+        ModBlocks.registerModBlocks();
     }
 }
